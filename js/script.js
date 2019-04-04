13d3cdd66a7b53b846e714308bcefa4e50fa5ad3
wendyc8035 <wendyc8035@queensvoc.org>
2019-04-04 22:28:01 +0000

    Day 2

--- js/script.js
+++ js/script.js
@@ -3,9 +3,15 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
-$("#search-button").click(function(){
-  
-  
-  
-});
-
+    $("#search-button").click(function(){
+      var value = $("input").val();
+      var img_url = "https://api.giphy.com/v1/gifs/search?q=" + value + "&rating=pg&api_key=dc6zaTOxFJmzC";
+    $.ajax({
+      url: img_url,
+      method: "GET",
+      success: function(response) {
+      var url = response.data[0].images.original.url;
+    $('body').append('<img src=' + url + '>');
+    },
+    });
+    });
